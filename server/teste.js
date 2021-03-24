81a55c6d52324805b8c5c9c8a8854c21d428bb33
Marcelo Augusto Pereira de Assis <71731452+marceloapd@users.noreply.github.com>
2021-03-24 19:29:43 -0300

    Delete teste.js

--- server/teste.js
+++
@@ -1,7 +0,0 @@
-// function matchString() {
-//     var string = "OVT1270";
-//     var result = string.match(/(\w)(\w)(\w)([0-9])([0-9])([0-9])([0-9])/)
-//     console.log("Saida  :", typeof result[0])
-// } matchString()
-const sinespApi = require('sinesp-api');
-
