6d53a7e9e6af400a15886cc448a2e2c7d86b61ef
Marcelo Assis <71731452+marceloapd@users.noreply.github.com>
2021-04-17 02:55:49 -0300

    backend para consumir a api do JoaoBidu

--- server/src/services/comandos/horoscopo.js
+++ server/src/services/comandos/horoscopo.js
@@ -1,24 +1,22 @@
 const inscrever = require("../../controllers/comandos").inscrever
+const request = require('request');
+const { JSDOM } = require('jsdom')
 
 async function run(comando, message, client){
-   let signo = message.body.split(" ")[1]
-    console.log(signo)
+    let signo = message.body.split(" ")[1].toLowerCase()
+    gerarHoroscopo(signo,message,client)
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+function gerarHoroscopo(signo,message,client){
+    request(`https://joaobidu.com.br/horoscopo/signos/previsao-${signo}`, function (error, response, body) {
+        const { document } = new JSDOM(body).window
+        let horoscopo = document.querySelector('.texto').querySelectorAll('p')
+        horoscopo = `${horoscopo[0].textContent} \n ${horoscopo[1].textContent}`
+        client.sendText(message.from, `_Aqui está o horoscopo do dia de ${signo.charAt(0).toUpperCase() + signo.slice(1)}, ${message.sender.pushname} 🧙‍♂️_ \n${horoscopo}`)
+});
+}
 
 inscrever("#horoscopo", run)
