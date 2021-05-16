a3eab7948064e7a0b696483ffb3f8e07c0f79b0f
marceloapd <marcelo.apdassis@gmail.com>
2021-05-16 14:30:18 -0300

    resize em fig animadas

--- server/src/services/comandos/figurinha.js
+++ server/src/services/comandos/figurinha.js
@@ -1,12 +1,18 @@
 const inscrever = require("../../controllers/comandos").inscrever
 const gify = require('gify')
 
+const { execFile } = require('child_process');
+const gifsicle = require('gifsicle');
+
+var sizeOf = require('image-size');
+
 let tipos_permitidos = [
     'image',
     'video'
 ]
 
 async function run(comando, message, client){
+
     try {
         if(tipos_permitidos.includes(message.type)){
             figurinha(message, client)
@@ -34,11 +40,24 @@ async function figurinha(message, client){
               };
             gify('./assets/images/copy.mp4', './assets/images/copy.gif',opts,function(err){
                 if (err) throw err;
-                client.sendImageAsStickerGif(message.from, "./assets/images/copy.gif")
-                console.log(`[${message.sender.id}] Figurinha criada`)
-                client.sendImageAsStickerGif(message.from, './assets/emojis/pixEmoji.gif')
-                enviarResposta("Aqui está sua Figurinha " + message.sender.pushname + ", não se esqueça de apoiar o meu desenvolvimento doando qualquer valor no PIX EMAIL: marcelo.apdassis@gmail.com", client, message)
-              })
+                var dimensions = sizeOf('./assets/images/copy.gif')
+                let crop = (dimensions.width - dimensions.height) / 2
+                if(dimensions.width < dimensions.height){
+                    execFile(gifsicle, ['--crop', `0,${parseInt(crop*-1)}+${dimensions.width}x${dimensions.width}`, '-o', './assets/images/resize.gif', './assets/images/copy.gif'], err => {
+                        client.sendImageAsStickerGif(message.from, "./assets/images/resize.gif")
+                        console.log(`[${message.sender.id}] Figurinha criada`)
+                        client.sendImageAsStickerGif(message.from, './assets/emojis/pixEmoji.gif')
+                        enviarResposta("Aqui está sua Figurinha " + message.sender.pushname + ", não se esqueça de apoiar o meu desenvolvimento doando qualquer valor no PIX EMAIL: marcelo.apdassis@gmail.com", client, message)
+                    });
+                }else{
+                    execFile(gifsicle, ['--crop', `${parseInt(crop)},0+${dimensions.height}x${dimensions.height}`, '-o', './assets/images/resize.gif', './assets/images/copy.gif'], err => {
+                        client.sendImageAsStickerGif(message.from, "./assets/images/resize.gif")
+                        console.log(`[${message.sender.id}] Figurinha criada`)
+                        client.sendImageAsStickerGif(message.from, './assets/emojis/pixEmoji.gif')
+                        enviarResposta("Aqui está sua Figurinha " + message.sender.pushname + ", não se esqueça de apoiar o meu desenvolvimento doando qualquer valor no PIX EMAIL: marcelo.apdassis@gmail.com", client, message)
+                      });
+                }
+            })
         }
         else{
             const base = await client.downloadMedia(message)
