35cd570184909e32ba30107fa4e8490ad83e4f5e
Matheus de Assis <matheusassismelo@gmail.com>
2021-07-09 00:21:03 -0300

    Comandos do privado corrigidos

--- server/src/controllers/comandos.js
+++ server/src/controllers/comandos.js
@@ -1,20 +1,27 @@
 let observadores = {}
 
+/**
+ * Executa o comando correto de acordo com o comando passado como parametro
+ * @param {string} comando comando que será usado para chamar a função correta 
+ * @param {object} message objeto da menssagem do cliente
+ * @param {object} client cliente venom
+ */
 function chamarComando(comando, message, client){
     let comandoFormatado = comando.split(" ")[0].toLowerCase()
     observadores[comandoFormatado](comando.toLowerCase(), message, client) 
 }
 
+
+/**
+ * Inscreve uma função em observadores que ficará disponível para uso
+ * @param {string} nomeComando nome que será usado para ativar a função
+ * @param {function} func funcao que será usada quando o comando for acionado
+ */
 function inscrever(nomeComando, func){
     observadores[nomeComando] = func
-}
-
-function deletar(func){
-    
 }
 
 module.exports = {
     chamarComando: chamarComando,
     inscrever,
-    deletar
 }
--- server/src/helpers/mensageiro.js
+++ server/src/helpers/mensageiro.js
@@ -1,15 +1,25 @@
+/**
+ * Retorna uma mensagem de erro para o cliente
+ * @param {object} message objeto da mensagem de um cliente
+ * @param {object} client objeto client venom
+ * @param {object} erro objeto de erro com as mensagens necessarias 
+ */
 function enviarMensagemError(message, client, erro){
     let texto;
     if (erro.status == "talvez"){
         texto = `Acredito que você quis dizer ${erro.message} 😄, lembre-se que se tiver em duvida sobre algum comando digite #comandos`
-    }else if(erro.status == "privado"){
-        texto = `Bom, ${message.sender.pushname}, eu não sei manipular este tipo de arquivo!`
-    }else{
-        texto = `😭 Desculpa, ${message.sender.pushname}, ainda não sou capaz de entender tudo oque voce diz, mas consigo fazer algumas coisas como`
+    } else {
+        texto = `😭 Desculpa, ${message.sender.pushname}, ainda não sou capaz de entender tudo oque voce diz!`
     }
     client.reply(message.from, texto, message.id)
 }
 
+/**
+ * 
+ * @param {object} message objeto da mensagem de um cliente 
+ * @param {object} client objeto client venom
+ * @param {string} texto texto que será enviado
+ */
 function enviarMensagem(message, client, texto){
     client.reply(message.from, texto, message.id)
 }
